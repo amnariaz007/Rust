cbfabe43a675f0388029ba38ea30395b634a3948
amnariaz007 <amanamana606@gmail.com>
2024-04-15 00:07:26 +0500

    commit

---
+++ newproject/target/debug/NewProject.d
@@ -0,0 +1,1 @@
+/Users/apple/Desktop/Rust/newproject/target/debug/NewProject: /Users/apple/Desktop/Rust/newproject/src/main.rs
---
+++ newproject/target/debug/deps/NewProject-683d71eeb85f6cbf.d
@@ -0,0 +1,5 @@
+/Users/apple/Desktop/Rust/NewProject/target/debug/deps/NewProject-683d71eeb85f6cbf: src/main.rs
+
+/Users/apple/Desktop/Rust/NewProject/target/debug/deps/NewProject-683d71eeb85f6cbf.d: src/main.rs
+
+src/main.rs:
