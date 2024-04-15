8aa137236c6a8fed5d8924ad692c6d7a15e393e5
amnariaz007 <amanamana606@gmail.com>
2024-04-16 03:21:30 +0500

    commit

--- newproject/src/main.rs
+++ newproject/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+//use std::io;
 
 // fn main() {
 //     println!("Hello, world!");
@@ -39,9 +39,11 @@ use std::io;
 //     //Array
 //     let arr = [1,2,3,4,5];
 //     println!("{}", arr[0]); 
+
 //     //Tuples
 //     let tup : (u32, bool, char) = (1, true, 'a');
 //     println!("tup = {}", tup.0); 
+//     println!("tup = {:?}", tup); 
 
 
 //     // console user input output
@@ -65,12 +67,39 @@ use std::io;
 //     println!(" The value of z is {z}")
 // }
 
-fn main () {
-   let x = return_function();
-   println!("X is this {}", x);
-}
+//return functions 
+
+// fn main () {
+//    let x = return_function();
+//    println!("X is this {}", x)
+// }
 
 
-fn return_function () -> u32 {
-    50 * 50 
+// fn return_function () -> u32 {
+//     50 * 50 
+// }
+
+
+//If else statements
+
+
+// fn main () {
+//     let no = 5;
+
+//     if no == 5 {
+//         println!("no is {}", no );
+//     }else {
+//         println!("no is not equal {}", no );
+//     }
+// }
+
+fn main (){
+
+    let name  = "Amna";
+
+    if name == "sana" {
+        println!("name is true {}", name );
+    }else{
+        println!("name is false and {}", name );
+    }
 }
