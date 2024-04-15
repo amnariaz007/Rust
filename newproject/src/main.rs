7c677c9bdb9e6688a3f2e20781c336725db22b2c
amnariaz007 <amanamana606@gmail.com>
2024-04-15 22:33:33 +0500

    commit

--- newproject/src/main.rs
+++ newproject/src/main.rs
@@ -32,6 +32,12 @@ fn main() {
     let FLOAT_VALUE = 10.5;
     println!("FLOAT_VALUE = {}", FLOAT_VALUE); 
 
+    let arr = [1,2,3,4,5];
+    println!("{}", arr[0]); 
+
+    let tup : (u32, bool, char) = (1, true, 'a');
+    println!("tup = {}", tup.0); 
+
 
 }
 
