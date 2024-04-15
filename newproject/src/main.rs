3941d71ee4d594bc11c39498321cebde96ae1ad3
amnariaz007 <amanamana606@gmail.com>
2024-04-15 22:48:44 +0500

    commit

--- newproject/src/main.rs
+++ newproject/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 // fn main() {
 //     println!("Hello, world!");
 // }
@@ -26,17 +28,27 @@ fn main() {
     let x : &str = "Amna";
     println!("A = {}", x);
 
+    //char
     let x : char = 'a';
     println!("A = {}", x); 
 
+    //Floating
     let FLOAT_VALUE = 10.5;
     println!("FLOAT_VALUE = {}", FLOAT_VALUE); 
 
+    //Array
     let arr = [1,2,3,4,5];
     println!("{}", arr[0]); 
-
+    //Tuples
     let tup : (u32, bool, char) = (1, true, 'a');
     println!("tup = {}", tup.0); 
+
+
+    // console user input output
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read");
+    println!("{}", input);
+    
 
 
 }
