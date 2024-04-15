0e9e62c98573b4bab8ef9bb80e4e4d9eb4ae1a77
amnariaz007 <amanamana606@gmail.com>
2024-04-15 21:37:05 +0500

    commit

--- newproject/src/main.rs
+++ newproject/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+fn main(){
+    let mut x = 5;
+    println!("x = {}", x);
+
+    x = x+1;
+    println!("x = {}", x);
+
+    {
+        let x = x+1;
+    println!("x = {}", x);
+    }
 }
+
