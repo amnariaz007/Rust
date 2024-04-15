c372f10400d911f902282a6ef5847b75a0038e33
amnariaz007 <amanamana606@gmail.com>
2024-04-15 22:21:34 +0500

    commit

--- newproject/src/main.rs
+++ newproject/src/main.rs
@@ -3,16 +3,35 @@
 // }
 
 
-fn main(){
-    let mut x = 5;
+// fn main(){
+//     let mut x = 5;
+//     println!("x = {}", x);
+
+//     x = x+1;
+//     println!("x = {}", x);
+
+//     {
+//         let x = x+1;
+//     println!("x = {}", x);
+//     }
+// }
+
+
+
+fn main() {
+
+    let x : u32 = 5;
     println!("x = {}", x);
 
-    x = x+1;
-    println!("x = {}", x);
+    let x : &str = "Amna";
+    println!("A = {}", x);
 
-    {
-        let x = x+1;
-    println!("x = {}", x);
-    }
+    let x : char = 'a';
+    println!("A = {}", x); 
+
+    let FLOAT_VALUE = 10.5;
+    println!("FLOAT_VALUE = {}", FLOAT_VALUE); 
+
+
 }
 
